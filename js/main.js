fe1820c5f36c55747de407f02bf168004400f5b8
allen-huang9 <ligerx066@gmail.com>
2020-11-11 17:40:06 -0800

    Shows all list in localStorage. #3

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ var $openNewRosterModalButton = document.querySelector('.new-roster-button');
 var $newRosterModalForm = document.querySelector('.new-roster-modal');
 var $createNewRosterForm = document.querySelector('.modal-form > form');
 var $rosterModalInput = document.querySelector('.roster-name');
+var $listDisplay = document.querySelector('.list-display-container');
 
 $homePageServantRoster.addEventListener('click', function (e) {
   viewSwap('servant-lists');
+  appendRosterList();
 });
 
 $homePageDmgCalculator.addEventListener('click', function (e) {
@@ -32,11 +34,61 @@ window.addEventListener('beforeunload', function (e) {
 });
 
 $createNewRosterForm.addEventListener('submit', function (e) {
+
   e.preventDefault();
   data.roster.name = $rosterModalInput.value;
   data.rosterLists.push(data.roster);
   $newRosterModalForm.className += ' hide';
+
+  $createNewRosterForm.reset();
 });
+
+function appendRosterList() {
+  if ($listDisplay.hasChildNodes()) {
+    var $childNode = document.querySelector('.roster-list-row');
+    $listDisplay.removeChild($childNode);
+  }
+
+  buildRosterListView(data.rosterLists);
+}
+
+function buildRosterListView(array) {
+
+  for (var i = 0; i < array.length; i++) {
+
+    var $rosterListRow = document.createElement('div');
+    $rosterListRow.setAttribute('class', 'roster-list-row');
+
+    var $rowColumn1 = document.createElement('div');
+    $rowColumn1.setAttribute('class', 'column-half');
+
+    var $rCol1P = document.createElement('p');
+    $rCol1P.setAttribute('class', 'character-list');
+    $rCol1P.textContent = array[i].name;
+
+    var $rowColumn2 = document.createElement('div');
+    $rowColumn2.setAttribute('class', 'column-half display-flex flex-jf-center flex-ai-center');
+
+    var $rowCol2ViewButton = document.createElement('div');
+    $rowCol2ViewButton.setAttribute('class', 'view-list-content-button');
+    $rowCol2ViewButton.textContent = 'View';
+
+    var $rowCol2DeleteButton = document.createElement('div');
+    $rowCol2DeleteButton.setAttribute('class', 'delete-list-button');
+    $rowCol2DeleteButton.textContent = 'Delete';
+
+    $rowColumn1.appendChild($rCol1P);
+
+    $rowColumn2.appendChild($rowCol2ViewButton);
+    $rowColumn2.appendChild($rowCol2DeleteButton);
+
+    $rosterListRow.appendChild($rowColumn1);
+    $rosterListRow.appendChild($rowColumn2);
+
+    $listDisplay.appendChild($rosterListRow);
+  }
+
+}
 
 function viewSwap(view) {
   for (var i = 0; i < $dataView.length; i++) {
